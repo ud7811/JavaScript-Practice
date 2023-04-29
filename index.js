d6c124975c2a6649369d92212bb74f2e919a2ebc
utkarshdubey18 <19194346-utkarshdubey18@users.noreply.replit.com>
2023-04-29 18:34:52 +0000

    Added constructor function

--- index.js
+++ index.js
@@ -16,4 +16,13 @@ person();
 //Higher Order Func: functions that take one or more functions as arguments or return a fuction. for eg: map(), filter(), reduce()
 let ary = [10,20,23];
 let newary = ary.map((element)=>element+10);
-console.log(newary)
+console.log(newary);
+
+//Constructor Function: Used to creating objects of similar properties and methods. They are invoked using the new keyword to create instances of objects.
+
+function Person( name, place){
+  this.name = name;
+  this.place = place;
+}
+let user1 = new Person("Utkarsh", 'Noida');
+console.log(`Hello everyone this is ${user1.name}, and i am from ${user1.place}`);
