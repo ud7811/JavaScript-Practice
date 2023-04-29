6b262f0c0c4c0c8df356b1cafc5cb50c6c517aae
utkarshdubey18 <19194346-utkarshdubey18@users.noreply.replit.com>
2023-04-29 17:57:41 +0000

    Named function, Anonymous function

---
+++ index.js
@@ -0,0 +1,11 @@
+// Named Function:
+function myFunction() {
+  console.log("Named Function");
+}
+myFunction();
+
+//Anonymous functions: without name, used as fubction expression or a arguments
+let greet = function (name){
+  console.log(`Hello ${name}`)
+};
+greet('Utkarsh')
